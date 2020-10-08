53e8f5cc24421139a720679406aed295981dd132
Balint <Balint@DESKTOP-T6GPV4L>
2020-10-08 19:36:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var wind = $(window);
 var sticky = $("#stickyHeader");
 wind.on('scroll', function() {
     var scroll = wind.scrollTop();
-    if (scroll < 100) {
+    if (scroll < 150) {
         sticky.removeClass('sticky-top');
     } else {
         sticky.addClass('sticky-top');
